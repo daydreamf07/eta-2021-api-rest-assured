916ae520987055ea9aa9c263b0c6e1c561fb5c61
Dayse Maria Marques Ferreira <dmmf@cesar-ldap.intranet>
2021-07-02 08:10:50 -0300

    post user tests done

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -4,6 +4,10 @@ public class Constants {
     public static final String MESSAGE_SUCCESS_LOGIN = "Login realizado com sucesso";
     public static final String MESSAGE_FAILED_LOGIN = "Email e/ou senha inválidos";
     public static final String MESSAGE_SUCCESS_USER_CREATION = "Cadastro realizado com sucesso";
+    public static final String MESSAGE_FAILED_USER_CREATION_EMAIL_ALREADY_IN_USE = "Este email já está sendo usado";
+    public static final String MESSAGE_FAILED_USER_CREATION_EMPTY_EMAIL = "email não pode ficar em branco";
+    public static final String MESSAGE_FAILED_USER_CREATION_EMPTY_NAME = "nome não pode ficar em branco";
+    public static final String MESSAGE_FAILED_USER_CREATION_EMPTY_PASSWORD = "password não pode ficar em branco";
+    public static final String MESSAGE_FAILED_USER_CREATION_WRONG_ADMIN = "administrador deve ser 'true' ou 'false'";
     public static final String MESSAGE_SUCCESS_PRODUCT_CREATION = "Cadastro realizado com sucesso";
-
 }
--- src/test/java/PostUserTest.java
+++ src/test/java/PostUserTest.java
@@ -10,10 +10,24 @@ import static requests.UserEndpoint.registerUserRequest;
 
 public class PostUserTest extends TestBase{
     private User validUser;
+    private User emailInUse;
+    private User emptyEmail;
+    private User emptyName;
+    private User emptyPassword;
+    private User emptyAdmin;
+    private User differentAdminValue;
+    private User allEmptyUser;
 
     @BeforeClass
     public void generateTestData(){
         validUser = new User("Tatu", "tatu@email.com", "123abc", "true");
+        emailInUse = new User("emailInUse", "tatu@email.com", "password123", "true");
+        emptyEmail = new User("EmptyEmail", "", "password123", "true");
+        emptyName = new User("", "emptyName@email.com", "password123", "true");
+        emptyPassword = new User("emptyPassword", "emptyPassword@email.com", "", "true");
+        emptyAdmin = new User("emptyAdmin", "emptyAdmin@email.com", "password123", "");
+        differentAdminValue = new User("differentAdminValue", "differentAdminValue@email.com", "password123", "value");
+        allEmptyUser = new User("", "", "", "");
     }
 
     @Test
@@ -25,5 +39,78 @@ public class PostUserTest extends TestBase{
                 statusCode(201).
                 body("message", equalTo(Constants.MESSAGE_SUCCESS_USER_CREATION)).
                 body("_id",notNullValue());
+    }
+
+    @Test
+    public void shouldNotCreateUserWithEmailAlreadyInUseAndReturnErrorMessageAndStatus400(){
+        Response registerUserRequest = registerUserRequest(SPEC, emailInUse);
+        registerUserRequest.
+                then().
+                assertThat().
+                statusCode(400).
+                body("message", equalTo(Constants.MESSAGE_FAILED_USER_CREATION_EMAIL_ALREADY_IN_USE));
+    }
+
+    @Test
+    public void shouldNotCreateUserWithEmptyEmailAndReturnErrorMessageAndStatus400(){
+        Response registerUserRequest = registerUserRequest(SPEC, emptyEmail);
+        registerUserRequest.
+                then().
+                assertThat().
+                statusCode(400).
+                body("email", equalTo(Constants.MESSAGE_FAILED_USER_CREATION_EMPTY_EMAIL));
+    }
+
+    @Test
+    public void shouldNotCreateUserWithEmptyNameAndReturnErrorMessageAndStatus400(){
+        Response registerUserRequest = registerUserRequest(SPEC, emptyName);
+        registerUserRequest.
+                then().
+                assertThat().
+                statusCode(400).
+                body("nome", equalTo(Constants.MESSAGE_FAILED_USER_CREATION_EMPTY_NAME));
+    }
+
+    @Test
+    public void shouldNotCreateUserWithEmptyPasswordAndReturnErrorMessageAndStatus400(){
+        Response registerUserRequest = registerUserRequest(SPEC, emptyPassword);
+        registerUserRequest.
+                then().
+                assertThat().
+                statusCode(400).
+                body("password", equalTo(Constants.MESSAGE_FAILED_USER_CREATION_EMPTY_PASSWORD));
+    }
+
+    @Test
+    public void shouldNotCreateUserWithEmptyAdminAndReturnErrorMessageAndStatus400(){
+        Response registerUserRequest = registerUserRequest(SPEC, emptyAdmin);
+        registerUserRequest.
+                then().
+                assertThat().
+                statusCode(400).
+                body("administrador", equalTo(Constants.MESSAGE_FAILED_USER_CREATION_WRONG_ADMIN));
+    }
+
+    @Test
+    public void shouldNotCreateUserWithAdminValueDifferentFromTrueOrFalseAndReturnErrorMessageAndStatus400(){
+        Response registerUserRequest = registerUserRequest(SPEC, differentAdminValue);
+        registerUserRequest.
+                then().
+                assertThat().
+                statusCode(400).
+                body("administrador", equalTo(Constants.MESSAGE_FAILED_USER_CREATION_WRONG_ADMIN));
+    }
+
+    @Test
+    public void shouldNotCreateUserWithAllRequiredFieldsEmptyAndReturnErrorMessageAndStatus400(){
+        Response registerUserRequest = registerUserRequest(SPEC, allEmptyUser);
+        registerUserRequest.
+                then().
+                assertThat().
+                statusCode(400).
+                body("nome", equalTo(Constants.MESSAGE_FAILED_USER_CREATION_EMPTY_NAME)).
+                body("email", equalTo(Constants.MESSAGE_FAILED_USER_CREATION_EMPTY_EMAIL)).
+                body("password", equalTo(Constants.MESSAGE_FAILED_USER_CREATION_EMPTY_PASSWORD)).
+                body("administrador", equalTo(Constants.MESSAGE_FAILED_USER_CREATION_WRONG_ADMIN));
     }
 }
